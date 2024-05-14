c05079bf03748e67dda6a9bf4be0d933ebc1bd2b
PC\PC_TTC <Tuankh.sctv@gmail.com>
2024-05-14 15:32:08 +0700

    Update TeduShop.Model (class Model)

---
+++ TeduShop.Model/Models/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string URL { get; set; }
+
+        
+        public int? DisplayOrder {  get; set; }
+
+        [Required]
+        public int GroupID { get; set; }
+
+        [ForeignKey("GroupID")]
+        public virtual MenuGroup MenuGroup { get; set; }
+        
+
+        public string Target { get; set; }
+
+        [Required]
+        public bool Status {  get; set; }
+    }
+}
---
+++ TeduShop.Model/Models/MenuGroup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual IEnumerable<Menu> Menus { get; set; }
+    }
+}
